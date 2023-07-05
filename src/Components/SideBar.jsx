7ed1617222556f75bddf41d96fefb7b592b8cd44
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-07-05 22:59:02 +0530

    onClick render of CalenderCol and NotesCol

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import NotesCol from "./NotesCol";
 import CalendarCol from "./CalendarCol";
 
@@ -12,16 +12,21 @@ export default function SideBar() {
   const handleCalendarClick = () => {
     setShowCalendar(!ShowCalendar);
   };
+
   return (
     <>
-      <div className="h-screen w-30 bg-red-900 text-slate-200">
-        <ul className="flex-col-reverse">
-          <button onClick={handleNotesClick}>Notes</button>
-          <button onClick={handleCalendarClick}>Calendar</button>
-        </ul>
+      <div style={{ display: "flex" }}>
+        <div className="h-screen w-32 bg-red-900 text-slate-200">
+          <ul className="flex flex-col ">
+            <button onClick={handleNotesClick}>Notes</button>
+            <button onClick={handleCalendarClick}>Calendar</button>
+          </ul>
+        </div>
+        <div>
+          {ShowCalendar && <CalendarCol />}
+        </div>
+        {ShowNotes && <NotesCol />}
       </div>
-      <div className={}>{<CalendarCol />}</div>
-      <div>{<NotesCol />}</div>
     </>
   );
 }
